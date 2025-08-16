be9c2b724f52240241520b1bf4277a28efc9bf8d
Zakhele <prince.feela2905@gmail.com>
2025-08-16 18:58:03 +0200

    .

--- jobconnect-sa/components/Footer.tsx
+++ jobconnect-sa/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-gray-900 text-gray-300 py-6 mt-12">
      <div className="container mx-auto text-center px-4 sm:px-6">
         <p className="text-sm">
-          © {new Date().getFullYear()} JobConnect-SA · Built with ❤️ using Next.js & Tailwind CSS
+          © {new Date().getFullYear()} JobConnect-SA · Built with ❤️ using Next.js & Tailwind CSS By Zakhele Prince Feela
         </p>
       </div>
     </footer>
